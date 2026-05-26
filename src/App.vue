5553c39a82133a8aa39558f62cea496a5cb618dd
desesperanza161 <desesperanza161@gmail.com>
2026-05-26 18:43:41 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <CanvasEditor />
+  </div>
+</template>
+
+<script setup lang="ts">
+import CanvasEditor from './components/CanvasEditor.vue'
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background: #f0f0f0;
+  font-family: sans-serif;
+}
+
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+</style>
---
+++ src/components/CanvasEditor(test).vue
@@ -0,0 +1,20 @@
+<template>
+<canvas ref="myCanvas" width="400" height="300"></canvas>
+</template>
+
+<script>
+export default{
+    mounted() {
+        // здесь ссылка на dom-element
+        const canvas = this.$refs.myCanvas;
+        // 2d для рисования
+        const ctx = canvas.getContext('2d');
+        // работа с изображением
+        const img= new Image();
+        img.src = 'https://placekitten.com/400/300';
+             img.onload = () => {
+            ctx.drawImage(img, 0,0, canvas.width, canvas.height);
+        }
+     }
+    };
+    </script>
